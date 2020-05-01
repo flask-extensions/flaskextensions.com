aae57eacfd1503bb225dab80d5ed983e68e5a0c6
Fabricio Aguiar <fabricio.aguiar@gmail.com>
2020-05-01 01:25:25 -0300

    Including fexapi on the CI (#51)

--- fexapi/tests/test_fexapi.py
+++ fexapi/tests/test_fexapi.py
@@ -1,5 +1,16 @@
+from fastapi.testclient import TestClient
+
 from fexapi import __version__
+from fexapi.api import app
+
+
+client = TestClient(app)
 
 
 def test_version():
     assert __version__ == "0.1.0"
+
+
+def test_read_main():
+    response = client.get("/docs")
+    assert response.status_code == 200
